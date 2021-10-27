96fbca2ad9004adfe8b18bffa17d1f4fc2779d28
seungwonleee <seungwon.code@gmail.com>
2021-10-27 16:37:51 +0900

    Passed the test code

--- src/Todo/TodoForm.js
+++ src/Todo/TodoForm.js
@@ -1,19 +1,32 @@
 import React, { useState, useCallback } from 'react';
 
-const TodoForm = () => {
+const TodoForm = ({ onInsert }) => {
   const [value, setValue] = useState('');
+
   const onChange = useCallback((e) => {
     setValue(e.target.value);
   }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      if (value.length === 0) {
+        return alert('글자를 입력해주세요.');
+      }
+      e.preventDefault();
+      onInsert(value);
+      setValue('');
+    },
+    [onInsert, value]
+  );
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <input
         type="text"
         placeholder="할 일을 입력하세요."
         value={value}
         onChange={onChange}
       />
-      <button>추가</button>
+      <button type="submit">추가</button>
     </form>
   );
 };
